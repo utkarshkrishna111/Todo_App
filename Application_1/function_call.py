2d91c4fc412cc70226442bc0928302c1e6d94264
Utkarsh Krishna <utkarsh_krishna@outlook.com>
2025-02-18 23:58:13 +0100

    Completed the Web App

--- Application_1/function_call.py
+++ Application_1/function_call.py
@@ -1,4 +1,4 @@
-def get_todos(filepath = "todo.txt"):
+def get_todos(filepath = r"C:\Users\utkar\PycharmProjects\PythonProject1\Application_1\todo.txt"):
     """ Read a text file and return a list of
     to-do items."""
     with open(filepath, 'r') as file_local:
@@ -6,7 +6,7 @@ def get_todos(filepath = "todo.txt"):
     return todos_local
 
 
-def write_todos(todos_arg, filepath = "todo.txt"):
+def write_todos(todos_arg, filepath = r"C:\Users\utkar\PycharmProjects\PythonProject1\Application_1\todo.txt"):
     """ Write the To-do items list in the text file. """
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
